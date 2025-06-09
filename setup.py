70fef54a34bb8d755affef3b7f9224306430c539
Howprobable <jannikeggert@web.de>
2025-06-09 20:48:49 +0200

    fix: update py_helpers reference and correct project URL in setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='browser_interface',
-    version='2.2.0',
+    version='2.2.1',
     packages=find_packages(),
     description='A python interface for a browser',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Jannik Eggert',
     author_email='eggertjannik@gmail.com',
-    url='https://github.com/howprobable/py-helpers',
+    url='https://github.com/howprobable/browser_interface',
     install_requires=[
         [line.strip() for line in open('requirements.txt').readlines() if line.strip() and not line.startswith('#')]
     ],
